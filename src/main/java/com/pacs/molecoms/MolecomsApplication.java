eaa0af3298eec954410cf305dd7751e16429ffa1
ranpia <richard.kim146@gmail.com>
2025-08-20 11:33:35 +0900

    initial

---
+++ src/main/java/com/pacs/molecoms/MolecomsApplication.java
@@ -0,0 +1,13 @@
+package com.pacs.molecoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolecomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolecomsApplication.class, args);
+	}
+
+}
